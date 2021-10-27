341b6493465acecee9264c2ddadf8668ed3c0ba1
Rouge <14047458+rougetimelord@users.noreply.github.com>
2021-10-27 00:20:18 -0700

    Add package files, add start script

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,13 @@ import Commands
 
 VERSION = "0.0.4"
 
+import logging
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    format="[%(name)s][%(levelname)s] %(message)s",
+    level=logging.INFO,
+)
+
 
 class Client(discord.Client):
     command_prefix = "👋"
@@ -197,6 +204,8 @@ class Client(discord.Client):
 
 def main():
     """Main method, starts everything glhf"""
+    log.info(f"DM Bot v{VERSION} started")
+
     intents = discord.Intents.default()
     intents.members = True
     intents.presences = True
@@ -212,11 +221,5 @@ def main():
         exit(1)
 
 
-if __name__ == "__main__":
-    log = logging.getLogger()
-    logging.basicConfig(
-        format="[%(name)s][%(levelname)s] %(message)s",
-        level=logging.INFO,
-    )
-    log.info(f"DM Bot v{VERSION} started")
+if __name__ == "__main__":   
     main()
